9470b4e24ebc52be02ac23456f91fac4f2eca32f
MinhTris245 <trantri04032004@gmail.com>
2025-04-09 20:54:51 +0700

    hệ thống có tùy chọn đăng nhập bằng google, facebook, icloud

--- MinhTri.php
+++ MinhTri.php
@@ -1,3 +1,4 @@
 US1: Là một người dung, tôi muốn hệ thống có tính năng lấy lại mật khẩu bằng gửi link qua gmail bảo mật abcabc
 US2: Là một người dùng, tôi muốn hệ thống có tính năng lọc sản phẩm theo khoản giá abc
 US3: Là một người dùng, tôi muốn hệ thống có tính năng gửi thông báo khi đăng nhập trên thiết bị mới 
+US4: Là một người dùng, tôi muốn hệ thống có tùy chọn đăng nhập bằng google, facebook, icloud
