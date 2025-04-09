4290417358b0d2b565aca01bde6e331cbd713004
MinhTris245 <trantri04032004@gmail.com>
2025-04-09 20:56:38 +0700

    hệ thống có tính năng chat với tư vấn viên

--- MinhTri.php
+++ MinhTri.php
@@ -2,3 +2,4 @@ US1: Là một người dung, tôi muốn hệ thống có tính năng lấy l�
 US2: Là một người dùng, tôi muốn hệ thống có tính năng lọc sản phẩm theo khoản giá abc
 US3: Là một người dùng, tôi muốn hệ thống có tính năng gửi thông báo khi đăng nhập trên thiết bị mới 
 US4: Là một người dùng, tôi muốn hệ thống có tùy chọn đăng nhập bằng google, facebook, icloud
+US5: Là một người dùng, tôi muốn hệ thống có tính năng chat với tư vấn viên
