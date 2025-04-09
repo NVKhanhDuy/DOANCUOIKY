3817df9ac92572312c15bf5ace22ddbad84f80d2
MRDUOCOK <ngoduoc86@gmail.com>
2025-04-09 18:19:36 +0700

    US5:Là một người dùng, tôi muốn nhận thông báo khuyến mãi qua email hoặc ứng dụng

--- ThanhDuoc.php
+++ ThanhDuoc.php
@@ -2,3 +2,4 @@ US1:Là một người dùng, tôi muốn hệ thống hiển thị đánh giá 
 US2:Là một người dùng, tôi muốn được chọn hình thức thanh toán như chuyển khoản, COD hoặc ví điện tử
 US3:Là một người dùng, tôi muốn theo dõi trạng thái giao hàng để biết đơn hàng của mình đang ở đâu
 US4:Là một người dùng, tôi muốn được đánh giá sản phẩm sau khi nhận hàng
+US5:Là một người dùng, tôi muốn nhận thông báo khuyến mãi qua email hoặc ứng dụng
